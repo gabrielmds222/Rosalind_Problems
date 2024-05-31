6c94973f12abf08924e6336e309a6a26d0d74d5d
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-05-31 11:33:10 -0300

    feat: Strings and Lists

---
+++ Python_Village/QuestionThree/main.py
@@ -0,0 +1,9 @@
+wordOneStart = 30
+wordOneEnd = 37
+
+wordTwoStart = 108
+wordTwoEnd = 115
+
+text = "nreSUkVV3NOEzP0IYtOoMoYBt7esCtPlataleaNjSm8XdyfaPXbpfWZsL7DrThTXnjLOnqbrnoAsBqyUuDsqDdT1NNpseNOI4p9nDJ2YobpDteguixinuJlXK7NeCALkyl617qtXICn4SdciH2Kx1jULkCkp82EBb7g2ida1N3s6I9oyq072DXfmzQeM."
+
+print(f"{text[wordOneStart:wordOneEnd + 1]} {text[wordTwoStart:wordTwoEnd + 1]}")
